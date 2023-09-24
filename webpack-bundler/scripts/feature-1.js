4f8ecb1b41165285e40ce82b1a49176aaa821a09
J4n <lmint.dev@gmail.com>
2023-09-24 21:51:19 +0700

    feat: update webpack code

--- webpack-bundler/scripts/feature-1.js
+++
@@ -1,5 +0,0 @@
-import { config, logConfig } from './core';
-
-config.feature_1 = 'Feature 1 config';
-
-logConfig('Feature 1');
--- webpack-bundler/src/scripts/core.js
+++ webpack-bundler/src/scripts/core.js
@@ -3,3 +3,5 @@ export const config = {};
 export function logConfig(from) {
   console.log(`Logging config from ${from}`, config);
 }
+
+console.log('core has been initialized successfully');
