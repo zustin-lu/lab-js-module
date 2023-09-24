5a5e4d435568662e1b2b5e9a5e6ddfaf2d11d0fb
J4n <lmint.dev@gmail.com>
2023-09-24 23:44:42 +0700

    chore: rename webpack esmodule example

--- webpack-bundler/src/scripts/core.js
+++
@@ -1,7 +0,0 @@
-export const config = {};
-
-export function logConfig(from) {
-  console.log(`Logging config from ${from}`, config);
-}
-
-console.log('core has been initialized successfully');
--- webpack/bundle-single-file/src/scripts/module-1.js
+++
@@ -1,4 +0,0 @@
-import { logConfig } from './core';
-
-console.log('--- From Module 1 ---');
-logConfig('Module 1');
